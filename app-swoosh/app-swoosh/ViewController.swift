b1f7b5994bea383db772b93e30f0b7f7d674bcfd
Dobs Totev <dobstotev@Dobss-MacBook-Pro.local>
2018-11-02 15:46:06 +0200

    Activating autolayout on the loading screen

--- app-swoosh/app-swoosh/ViewController.swift
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -9,12 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var swooshLogo: UIImageView!
+    @IBOutlet weak var bgImage: UIImageView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
     }
-
-
 }
 
